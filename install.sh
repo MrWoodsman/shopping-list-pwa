d6d87989607431cfce339d4577254ccb2a6cfee2
MrWoodsman <mikolaj.cwikla12@gmail.com>
2026-07-01 23:14:08 +0200

    poprawaka install.sh

--- install.sh
+++ install.sh
@@ -31,4 +31,17 @@ EOF
 systemctl daemon-reload
 systemctl enable --now shopping-app.service
 
+# Tworzy komendę 'shopping-logs', która otwiera logi
+echo "alias shopping-logs='journalctl -u shopping-app -f'" >> /root/.bashrc
+
+echo "--------------------------------------------------------"
+echo "Instalacja zakończona pomyślnie!"
+echo "Twoja aplikacja działa jako usługa: shopping-app"
+echo ""
+echo "Przydatne komendy:"
+echo " - Status:   systemctl status shopping-app"
+echo " - Logi:     journalctl -u shopping-app -f"
+echo " - Restart:  systemctl restart shopping-app"
+echo "--------------------------------------------------------"
+
 echo "Gotowe! Apka działa pod adresem IP kontenera na porcie 3000."
