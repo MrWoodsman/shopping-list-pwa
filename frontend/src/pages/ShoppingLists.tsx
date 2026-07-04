e02b4a0005ad7e4bf52e65bac9b11847295b3f79
MrWoodsman <mikolaj.cwikla12@gmail.com>
2026-07-04 15:33:54 +0200

    Konfiguracja Tanstack Query

--- frontend/src/pages/ShoppingLists.tsx
+++ frontend/src/pages/ShoppingLists.tsx
@@ -13,29 +13,7 @@ export interface ShoppingListData {
 export function ShoppingLists() {
   const [searchInput, setSearchInput] = useState("");
   // setShoppingListsData
-  const [shoppingListsData] = useState([
-    {
-      id: 162765436543,
-      name: "Pierwsza lista",
-      createdAt: "02.07.2026",
-      itemsIn: 4,
-      completedCount: 1,
-    },
-    {
-      id: 162765436544,
-      name: "Obiad niedziela",
-      createdAt: "02.07.2026",
-      itemsIn: 6,
-      completedCount: 2,
-    },
-    {
-      id: 162765436545,
-      name: "Zupa Meksykańska",
-      createdAt: "02.07.2026",
-      itemsIn: 10,
-      completedCount: 0,
-    },
-  ]);
+  const [shoppingListsData] = useState([]);
 
   return (
     <div className="w-full h-full flex flex-col gap-2">
