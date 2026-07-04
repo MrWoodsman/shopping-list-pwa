b2ceaf4db7a1c72a5d5423c8ed7238c80da255ab
MrWoodsman <mikolaj.cwikla12@gmail.com>
2026-07-04 17:01:51 +0200

    Rozbudowa ekranu listy

--- frontend/src/pages/ShoppingScreen.tsx
+++ frontend/src/pages/ShoppingScreen.tsx
@@ -1,3 +1,6 @@
+import { useParams, Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+// UI
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,26 +9,88 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { useParams } from "react-router-dom";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+// TYPES
+import { type ShoppingListData } from "@shared/types";
 
 export function ShoppingScreen() {
   const { id } = useParams();
 
   console.log(`Pobieranie danych o ${id}`);
 
+  const { data, isLoading, error } = useQuery<ShoppingListData>({
+    queryKey: ["shoppingList", id],
+    queryFn: async () => {
+      const response = await fetch(`/api/shopping-lists/${id}`);
+      if (!response.ok) throw new Error("Błąd pobierania");
+      return response.json();
+    },
+    enabled: !!id, // Zapytanie wykona się tylko jeśli id istnieje
+  });
+
+  if (isLoading) return <div>Ładowanie szczegółów...</div>;
+  if (error || !data) return <div>Nie znaleziono listy.</div>;
+
   return (
     <div className="shopping-lists-list h-full space-y-2 px-2 overflow-y-auto scrollbar-gutter-stable pt-[max(8px,env(safe-area-inset-top))]">
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
-            <BreadcrumbLink href="//shopping-lists">Lista</BreadcrumbLink>
+            <BreadcrumbLink asChild>
+              <Link to="/shopping-lists">Lista</Link>
+            </BreadcrumbLink>
           </BreadcrumbItem>
           <BreadcrumbSeparator />
           <BreadcrumbItem>
-            <BreadcrumbPage>LIST NAME {id}</BreadcrumbPage>
+            <BreadcrumbPage>{data.name}</BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+
+      <Accordion type="multiple" defaultValue={["to_buy"]}>
+        <AccordionItem value="purchased">
+          <AccordionTrigger>Kupione</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-2">
+            {/* LISTA JUZ KUPIONYCH PRZEDMIOTÓW */}
+            <div className="bg-neutral-500 p-2 rounded-2xl">
+              <h1>test</h1>
+            </div>
+            <div className="bg-neutral-500 p-2 rounded-2xl">
+              <h1>test</h1>
+            </div>
+            <div className="bg-neutral-500 p-2 rounded-2xl">
+              <h1>test</h1>
+            </div>
+            <div className="bg-neutral-500 p-2 rounded-2xl">
+              <h1>test</h1>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="to_buy">
+          <AccordionTrigger>Do kupienia</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-2">
+            {/* LISTA JUZ KUPIONYCH PRZEDMIOTÓW */}
+            <div className="bg-neutral-500 p-2 rounded-2xl">
+              <h1>test</h1>
+            </div>
+            <div className="bg-neutral-500 p-2 rounded-2xl">
+              <h1>test</h1>
+            </div>
+            <div className="bg-neutral-500 p-2 rounded-2xl">
+              <h1>test</h1>
+            </div>
+            <div className="bg-neutral-500 p-2 rounded-2xl">
+              <h1>test</h1>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      {/* <p>Data utworzenia: {data.createdAt}</p> */}
     </div>
   );
 }
