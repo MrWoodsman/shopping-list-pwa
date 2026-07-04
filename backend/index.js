f16a956fc82702f5a2bc0cfa13556bc3d1ab94ed
MrWoodsman <mikolaj.cwikla12@gmail.com>
2026-07-04 15:31:10 +0200

    Dodanie danych testowych oraz api

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,38 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// TESTOWE DANE
+const shoppingLists = [
+  {
+    id: 162765436543,
+    name: "Pierwsza lista",
+    createdAt: "02.07.2026",
+    itemsIn: 4,
+    completedCount: 1,
+  },
+  {
+    id: 162765436544,
+    name: "Obiad niedziela",
+    createdAt: "02.07.2026",
+    itemsIn: 6,
+    completedCount: 2,
+  },
+  {
+    id: 162765436545,
+    name: "Zupa Meksykańska",
+    createdAt: "02.07.2026",
+    itemsIn: 10,
+    completedCount: 0,
+  },
+];
+
 // 1. Twoje API (musi być przed statycznymi plikami!)
 app.get("/api/test", (req, res) => {
   res.json({ message: "Działa!" });
+});
+
+app.get("/api/shopping-lists", (req, res) => {
+  res.json(shoppingLists);
 });
 
 // 2. Serwowanie zbudowanego frontendu
