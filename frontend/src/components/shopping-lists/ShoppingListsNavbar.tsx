69f23a42bdbc7fc0f57c84a0a28c064cc84bbc7e
MrWoodsman <mikolaj.cwikla12@gmail.com>
2026-07-16 22:10:29 +0200

    Dodanie przycisku pod ekran pokazujacy wszystkie listy razem

--- frontend/src/components/shopping-lists/ShoppingListsNavbar.tsx
+++ frontend/src/components/shopping-lists/ShoppingListsNavbar.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ButtonGroup } from "@/components/ui/button-group";
 import { ListAddOverlay } from "../ListAddOverlay";
-import { Plus } from "lucide-react";
+import { Package, Plus, Search } from "lucide-react";
 
 type ShoppingListsNavbarProps = {
   inputVal: string;
@@ -14,21 +14,26 @@ export function ShoppingListsNavbar({ inputVal, setInputVal }: ShoppingListsNavb
     <div className="search-container flex gap-2 px-2 pt-[max(8px,env(safe-area-inset-top))]">
       <ButtonGroup aria-label="Button group" className="w-full">
         <ButtonGroup className="w-full">
-          <Button variant={"outline"}>Szukaj</Button>
+          <Button variant={"outline"} size={"icon"}>
+            <Search className="size-4" />{" "}
+          </Button>
           <Input
             type="text"
             value={inputVal}
             onInput={(e) => setInputVal(e.currentTarget.value)}
-            placeholder="Wyszukaj liste"
+            placeholder="Szukaj"
           />
         </ButtonGroup>
-        <ButtonGroup>
+        <div className="flex gap-2">
+          <Button variant="secondary" onClick={(e) => e.currentTarget.blur()}>
+            Razem <Package className="size-4" />
+          </Button>
           <ListAddOverlay>
             <Button variant="default" onClick={(e) => e.currentTarget.blur()}>
               Dodaj <Plus className="size-4" />
             </Button>
           </ListAddOverlay>
-        </ButtonGroup>
+        </div>
       </ButtonGroup>
     </div>
   );
