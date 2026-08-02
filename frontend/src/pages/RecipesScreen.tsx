204f8a5e0f82334feab4b18838a0b3fdf87ba77e
MrWoodsman <mikolaj.cwikla12@gmail.com>
2026-08-02 22:04:44 +0200

    Fix - problemu scrolowania listy przepisów

--- frontend/src/pages/RecipesScreen.tsx
+++ frontend/src/pages/RecipesScreen.tsx
@@ -46,7 +46,7 @@ export function RecipesScreen() {
           draftsCount={draftsCount}
         />
 
-        <div className="content flex-1 flex flex-col px-2 pb-2 gap-2 overflow-y-auto">
+        <div className="content flex-1 flex flex-col px-2 pb-2 gap-2 overflow-y-scroll">
           {displayedRecipes.length > 0 ? (
             displayedRecipes.map((recipe) => (
               <RecipeCard key={recipe.id} recipe={recipe} setRecipeToDelete={setRecipeToDelete} />
@@ -96,7 +96,7 @@ function RecipeCard({
     <Card
       key={recipe.id}
       onClick={() => navigate(ROUTES.RECIPES_VIEW(recipe.id))}
-      className="group relative flex flex-col overflow-hidden cursor-pointer transition-all hover:shadow-md hover:border-primary/30 pt-0 pb-0"
+      className="group relative flex flex-col shrink-0 overflow-hidden cursor-pointer transition-all hover:shadow-md hover:border-primary/30 pt-0 pb-0"
     >
       {/* SEKCJA ZDJĘCIA (Przeniesiona na górę dla lepszego efektu) */}
       <div className="relative aspect-video w-full overflow-hidden bg-muted">
